5f21ad7426ab05305b2131d78f03b0db6d25a879
Thomaz F B Cortez <thomazfcortez@gmail.com>
2014-11-17 12:10:37 -0300

    Code Upload

--- HashDictionary/wordTreeIterator.c
+++ HashDictionary/wordTreeIterator.c
@@ -7,4 +7,35 @@
 //
 
 #include "wordTreeIterator.h"
+#include "file.h"
 
+
+void doJump(FILE * indexedDictionary)
+{
+    int numberOfChilds;
+    fseek(indexedDictionary, (sizeof(char) + sizeof(int)), SEEK_CUR);
+    fread(&numberOfChilds, sizeof(int), 1, indexedDictionary);
+    for(int i = 0; i < numberOfChilds; i++)
+    {
+        fseek(indexedDictionary, (sizeof(char) + sizeof(int)), SEEK_CUR);
+    }
+}
+
+void jumpStructAmount(int amount, FILE * indexedDictionary)
+{
+    setFileRegisterToStartPoint(indexedDictionary);
+    for(int i = amount; i < amount; i++)
+    {
+        doJump(indexedDictionary);
+    }
+}
+
+void getWordPosition(char * buffer, int layer, FILE * indexedDictionary)
+{
+    int arraySize = layer - 1;
+    
+    for(int i = 0; i < layer; i++)
+    {
+        
+    }
+}
